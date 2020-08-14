e7a31bd5fc63888ce78d485738ddb01ff99a470c
Michael Rabon <nobar86@gmail.com>
2020-08-14 17:44:29 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,6 +8,8 @@ class GetRequester
 
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
+  def initialize
+
 
   def get_response_body
     uri = URI.parse(URL)
