8255b9a071aa26a7a8d8efc1ba170f11cc26f609
Michael Rabon <nobar86@gmail.com>
2020-08-14 17:49:57 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+
+
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def initialize(url)
+    @url = url
+  end
+
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    x = JSON.parse(self.get_response_body)
+  end
+
+end
